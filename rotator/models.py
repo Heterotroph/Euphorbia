63e95857fb84a6ce3caf926b0f10388fdb10cd5b
Igor Zygin <izygin@gmail.com>
2015-10-17 14:06:28 +0300

    added pixel forms

--- rotator/models.py
+++ rotator/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 __author__ = 'igorzygin'
@@ -26,4 +27,13 @@ class UserSite(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     created = models.DateTimeField(default=datetime.now())
+
+
+class UserPixel(models.Model):
+    site = models.ForeignKey(UserSite, unique=False)
+    name = models.CharField(max_length=100, unique=True)
+    unique_code = models.CharField(max_length=300, unique=True)
+
+    def generate_unique_code(self):
+        return uuid.uuid4().hex[:15].upper()
 
