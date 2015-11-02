95d3644512360a8106fe5d594e1bec67bf675d28
Igor Zygin <igorzygin@Igors-iMac.local>
2015-11-02 08:56:59 +0200

    asd

--- rotator/models.py
+++ rotator/models.py
@@ -6,22 +6,6 @@ __author__ = 'igorzygin'
 
 
 
-class Campaign(models.Model):
-    user = models.ForeignKey(User)
-    created = models.DateTimeField(blank=True, default=datetime.now())
-    name = models.CharField(max_length=30)
-
-
-class Banner(models.Model):
-    campaign = models.ForeignKey(Campaign)
-    banner_url = models.CharField(max_length=1000)
-
-
-class ImageBanner(Banner):
-    image_format = models.PositiveSmallIntegerField(blank=True, default=0)
-    image = models.ImageField(blank=False)
-
-
 class UserSite(models.Model):
     user = models.ForeignKey(User, unique=False)
     name = models.CharField(max_length=100)
