428ab6863df7c3ea869880402da71d17db681fba
Igor Zygin <igorzygin@Igors-iMac.local>
2015-11-02 18:02:29 +0200

    rotator models

--- rotator/admin.py
+++ rotator/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import UserPixel, UserSite
+from .models import UserPixel, UserSite, AdSpot, Campaign, Banner
 
 admin.site.register(UserPixel)
 admin.site.register(UserSite)
+admin.site.register(AdSpot)
+admin.site.register(Campaign)
+admin.site.register(Banner)
