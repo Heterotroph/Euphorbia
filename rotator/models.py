9799394ec47c8823acfb163d804c0a0955a18134
Igor Zygin <igorzygin@Igors-iMac.local>
2015-11-03 13:13:41 +0200

    AdThematics

--- rotator/models.py
+++ rotator/models.py
@@ -8,6 +8,14 @@ __author__ = 'igorzygin'
 def generate_unique_code():
     return uuid.uuid4().hex[:15].upper()
 
+
+class AdThematics(models.Model):
+    def __unicode__(self):
+        return self.name
+    created = models.DateTimeField(default=datetime.now)
+    name = models.CharField(max_length=100, blank=False)
+
+
 class UserSite(models.Model):
     def __unicode__(self):
         return self.name+" "+self.url
@@ -15,12 +23,12 @@ class UserSite(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     created = models.DateTimeField(default=datetime.now)
+    thematics = models.ManyToManyField(AdThematics, blank=True)
 
 
 class UserPixel(models.Model):
     def __unicode__(self):
         return self.name+" "+self.unique_code
-
     site = models.ForeignKey(UserSite, unique=False)
     name = models.CharField(max_length=100, unique=True)
     unique_code = models.CharField(max_length=300, unique=True, default=generate_unique_code)
@@ -47,6 +55,7 @@ class Campaign(models.Model):
     created = models.DateTimeField(default=datetime.now)
     name = models.CharField(max_length=100, unique=True)
     user = models.ForeignKey(User, unique=False)
+    thematics = models.ManyToManyField(AdThematics, blank=True)
 
 class Banner(models.Model):
     def __unicode__(self):
@@ -55,4 +64,7 @@ class Banner(models.Model):
     campaign = models.ForeignKey(Campaign)
     image = models.ImageField()
     format = models.SmallIntegerField(blank=False)
+
+
+
 
