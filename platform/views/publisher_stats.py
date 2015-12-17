ea8439b0692faad8f478e692bbc4ed4c6380881b
Igor Zygin <izygin@gmail.com>
2015-12-17 15:16:39 +0600

    publisher stats

--- platform/views/publisher_stats.py
+++ platform/views/publisher_stats.py
@@ -4,20 +4,26 @@ from platform.views.other import get_data_from_sql
 __author__ = 'igor'
 
 
-def get_publisher_stats(request):
-    data = []
-    sites_list = UserSite.objects.filter(user=request.user)
+def get_publisher_stats(user, from_date, to_date):
+    sites_list = UserSite.objects.filter(user=user)
     site_id_list = [site.id for site in sites_list]
-    query = "select pa.site_id as site_id, count(bss) as shows, count(bc) as clicks \
-            from platform_adspot as pa \
-            left join \
+    site_id_list_str = ",".join(site_id_list)
+
+
+    date_diff = (to_date-from_date)/(3600*24)
+    query = "select CURRENT_DATE - s.a AS date, pa.site_id as site_id, count(bss) as shows, count(bc) as clicks \
+            from Generate_series(%d, 0, -1) AS s(a) \
+            inner join  \
+            platform_adspot as pa \
+            on 1=1 \
+            left join  \
             banner_show_stats as bss \
-            on bss.spot_code = pa.unique_code \
+            on bss.spot_code = pa.unique_code and date(bss.created)=CURRENT_DATE - s.a \
             left join  \
             banner_clicks as bc \
-            on bc.spot_code = pa.unique_code \
-            where pa.site_id in (2) \
-            group by pa.site_id;"
+            on bc.spot_code = pa.unique_code and date(bss.created)=CURRENT_DATE - s.a \
+            where pa.site_id in (%s) \
+            group by pa.site_id,CURRENT_DATE - s.a;" % (date_diff, site_id_list_str)
 
     data = get_data_from_sql(query)
     for entry in data:
@@ -25,5 +31,5 @@ def get_publisher_stats(request):
             if site.id == entry['site_id']:
                 entry['site'] = site
                 break
-                
+
     return data
